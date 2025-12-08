e4a6ae614b2c11b32074ceea91b8f1c645472ffc
michaeldebetaz <netarion@outlook.com>
2025-12-08 22:25:55 +0100

    feat: finished day 04

--- 04-printing-departement/main.py
+++ 04-printing-departement/main.py
@@ -34,17 +34,8 @@ def count_adjacent_rolls(lines: list[list[str]], row: int, col: int) -> int:
     return count
 
 
-def main():
-    input = ""
-    with open("elsainput.txt", "r") as file:
-        input = file.read().strip()
-
-    lines: list[list[str]] = []
-    for line in input.splitlines():
-        lines.append([char for char in line])
-
+def positions_to_remove(lines: list[list[str]]) -> list[tuple[int, int]]:
     positions: list[tuple[int, int]] = []
-    rolls_count = 0
     for row in range(len(lines)):
         line = lines[row]
         for col in range(len(line)):
@@ -52,17 +43,37 @@ def main():
             if spot == ROLL:
                 count = count_adjacent_rolls(lines, row, col)
                 if count < 4:
-                    rolls_count += 1
                     positions.append((row, col))
 
-    for position in positions:
-        row, col = position
-        lines[row][col] = "X"
+    return positions
 
-    for line in lines:
-        print("".join(line))
 
-    print("Result 1: ", rolls_count)
+def main():
+    input = ""
+    with open("input.txt", "r") as file:
+        input = file.read().strip()
+
+    lines: list[list[str]] = []
+    for line in input.splitlines():
+        lines.append([char for char in line])
+
+    to_remove_1 = positions_to_remove(lines)
+    count_1 = len(to_remove_1)
+    print("Result 1: ", count_1)
+
+    to_remove: list[tuple[int, int]] = []
+    positions: list[tuple[int, int]] = []
+    first_iteration = True
+    while first_iteration or len(positions) > 0:
+        first_iteration = False
+        positions = positions_to_remove(lines)
+        for position in positions:
+            to_remove.append(position)
+            row, col = position
+            lines[row][col] = "."
+
+    count_2 = len(to_remove)
+    print("Result 2: ", count_2)
 
     return
 
