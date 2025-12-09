acedb11b879b56ba0998de71e4387435fbb77361
michaeldebetaz <netarion@outlook.com>
2025-12-09 22:13:06 +0100

    finished day 05

--- 05-cafeteria/main.py
+++ 05-cafeteria/main.py
@@ -1,9 +1,49 @@
+def is_in_ranges(id: int, ranges: list[tuple[int, int]]) -> bool:
+    for start, end in ranges:
+        if start <= id <= end:
+            return True
+    return False
+
+
 def main():
     text: str = ""
-    with open("test.txt", "r", encoding="utf-8") as file:
+    with open("input.txt", "r") as file:
         text = file.read()
 
-    print(text)
+    ranges_str, ids_str = text.split("\n\n")
+
+    ranges: list[tuple[int, int]] = []
+
+    for r in ranges_str.splitlines():
+        start, end = r.split("-")
+        ranges.append((int(start), int(end)))
+
+    fresh_ids: list[int] = []
+    for id_str in ids_str.splitlines():
+        id = int(id_str)
+        if is_in_ranges(id, ranges):
+            fresh_ids.append(id)
+
+    print(f"Result 1: {len(fresh_ids)}")
+
+    fresh_id_count = 0
+    ranges.sort()
+    seen_ranges: list[tuple[int, int]] = []
+    for r in ranges:
+        start, end = r
+        for seen_r in seen_ranges:
+            seen_start, seen_end = seen_r
+            if seen_start <= start <= seen_end:
+                start = seen_end + 1
+            if seen_start <= end <= seen_end:
+                end = seen_start - 1
+        count = end + 1 - start
+        if count <= 0:
+            continue
+        seen_ranges.append((start, end))
+        fresh_id_count += count
+
+    print(f"Result 2: {fresh_id_count}")
 
 
 if __name__ == "__main__":
