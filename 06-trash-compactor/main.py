58b9f48fea017b29b2588f7b4f27be4daf1aa7a4
michaeldebetaz <netarion@outlook.com>
2025-12-13 13:29:54 +0100

    finished day 06

--- 06-trash-compactor/main.py
+++ 06-trash-compactor/main.py
@@ -1,29 +1,73 @@
 def main():
     input: str = ""
-    with open("test.txt", "r") as file:
-        input = file.read().strip()
+    with open("input.txt", "r") as file:
+        input = file.read()
 
     lines = input.splitlines()
 
     last_line = lines[-1]
+    ops: list[str] = []
     col_ranges: list[tuple[int, int]] = []
     start = 0
     end = 0
-    for char in last_line[1:]:
-        if char == " ":
-            end += 1
-        else:
+    for i, char in enumerate(last_line):
+        if i == 0:
+            ops.append(char)
+            continue
+        if char != " ":
+            ops.append(char)
+            end = i - 2
             col_ranges.append((start, end))
-            start = end + 1
-            end += 1
+            start = i
+        if i == len(last_line) - 1:
+            end = i
+            col_ranges.append((start, end))
 
-    ops: list[str] = []
+    cols_1: list[list[int]] = []
     for start, end in col_ranges:
-        ops.append(last_line[start : start + 1])
+        col: list[int] = []
+        for line in lines[:-1]:
+            digits = line[start : end + 1].strip()
+            col.append(int(digits))
 
-    print(ops)
-    print(input)
-    print(col_ranges)
+        cols_1.append(col)
+
+    total_1 = 0
+    for i, op in enumerate(ops):
+        col = cols_1[i]
+        if op == "*":
+            result = 1
+            for num in col:
+                result *= num
+            total_1 += result
+        elif op == "+":
+            total_1 += sum(col)
+
+    print("Result 1: ", total_1)
+
+    cols_2: list[list[int]] = []
+    for start, end in col_ranges:
+        col: list[int] = []
+        for i in range(start, end + 1):
+            digits = ""
+            for line in lines[:-1]:
+                digits += line[i]
+            col.append(int(digits.strip()))
+
+        cols_2.append(col)
+
+    total_2 = 0
+    for i, op in enumerate(ops):
+        col = cols_2[i]
+        if op == "*":
+            result = 1
+            for num in col:
+                result *= num
+            total_2 += result
+        elif op == "+":
+            total_2 += sum(col)
+
+    print("Result 2: ", total_2)
 
     return
 
---
+++ 07-laboratories/main.py
@@ -0,0 +1,12 @@
+def main():
+    input = ""
+    with open("test.txt", "r") as f:
+        input = f.read()
+
+    print(input)
+
+    return
+
+
+if __name__ == "__main__":
+    main()
