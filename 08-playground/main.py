65d07a63d8884dd783e43b17d40242e4344562b9
michaeldebetaz <netarion@outlook.com>
2025-12-14 17:17:13 +0100

    finished day 08 part 1

--- 08-playground/main.py
+++ 08-playground/main.py
@@ -26,30 +26,78 @@ def find_closest_position(src: Position, positions: list[Position]) -> Position:
 
 def main():
     input = ""
-    with open("test.txt", "r") as f:
+    with open("input.txt", "r") as f:
         input = f.read()
 
     lines = input.splitlines()
-    positions: list[tuple[int, int, int]] = []
+    positions: list[Position] = []
     for line in lines:
         x, y, z = line.split(",")
         positions.append((int(x), int(y), int(z)))
 
-    pairs: list[tuple[Position, Position]] = []
+    distances: list[tuple[Position, Position, float]] = []
+    seen: set[tuple[Position, Position]] = set()
+    for pos_1 in positions:
+        for pos_2 in positions:
+            if pos_1 == pos_2:
+                continue
+            if (pos_1, pos_2) in seen or (pos_2, pos_1) in seen:
+                continue
+            seen.add((pos_1, pos_2))
+            dist = euclidian_distance(pos_1, pos_2)
+            distances.append((pos_1, pos_2, dist))
+
+    distances.sort(key=lambda x: x[2])
+
+    circuits: list[set[Position]] = []
+
+    for d in distances[:1_000]:
+        pos_1, pos_2, _ = d
+        if len(circuits) == 0:
+            circuits.append(set([pos_1, pos_2]))
+            continue
+
+        found = False
+        for circuit in circuits:
+            if pos_1 in circuit or pos_2 in circuit:
+                found = True
+                circuit.update(set((pos_1, pos_2)))
+                break
+
+        if not found:
+            circuits.append(set([pos_1, pos_2]))
 
     current_idx = 0
-    other_positions = positions.copy()
-    while True:
-        pos_1 = positions[current_idx]
-        other_positions.remove(pos_1)
-        pos_2 = find_closest_position(pos_1, other_positions)
-        pos_2_idx = positions.index(pos_2)
-        pairs.append((pos_1, pos_2))
-        current_idx = pos_2_idx
-        if len(other_positions) == 1:
-            break
+    while current_idx < len(circuits):
+        circuit = circuits[current_idx]
+        for c in circuits:
+            if circuit != c:
+                intersect = len(circuit.intersection(c)) > 0
+                if intersect:
+                    circuit.update(c)
+                    circuits.remove(c)
+                    current_idx = -1
+                    break
+        current_idx += 1
 
-    print(pairs)
+    for pos in positions:
+        for circuit in circuits:
+            if pos in circuit:
+                break
+        else:
+            circuits.append(set([pos]))
+
+    circuits.sort(key=lambda circuit: len(circuit), reverse=True)
+
+    # for c in circuits[:10]:
+    #     print(len(c))
+    #     print(c)
+
+    total = 1
+    for c in circuits[:3]:
+        total *= len(c)
+
+    print("Result 1:", total)
 
     return
 
